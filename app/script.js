ec38ecdeb2e8a6e46ca51315415b1b5145683994
zenmatcha <120019080+zenmatcha@users.noreply.github.com>
2024-01-27 18:18:34 +0100

    added everything but hamburger menu animation is not taking idk why

--- app/script.js
+++ app/script.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+const btnHamburger = document.querySelector("#btnHamburger");
+btnHamburger.addEventListener("click", function () {
+  console.log("click hamburger");
+
+  if (btnHamburger.classList.contains("open")) {
+    btnHamburger.classList.remove("open");
+  } else {
+    btnHamburger.classList.add("open");
+  }
+});
